e7354224b8a8330afaafa0f7f8f13a95fbb0269b
ppszczepaniak <piotr.p.szczepaniak@gmail.com>
2019-09-21 12:10:18 +0200

    create main method

--- src/main/java/BookOderApp.java
+++ src/main/java/BookOderApp.java
@@ -1,5 +1,28 @@
-public class BookOderApp {
-    public static void main(String[] args) {
-        System.out.println("Hello there!");
+import fi.iki.elonen.NanoHTTPD;
+
+import java.io.IOException;
+
+public class BookOderApp extends NanoHTTPD {
+
+
+    private BookOderApp(int port) throws IOException {
+        super(port);
+        start(5000, false); //daemon: should server start as a separate thread?
+        System.out.println("I've started your server...");
     }
+
+    public static void main(String[] args) {
+
+        try {
+            new BookOderApp(8080);
+        } catch (IOException e) {
+            System.err.println("Can't start your server! Error: \n" + e);
+        }
+    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        return null;
+    }
+
 }
